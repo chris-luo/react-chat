fa7057767c8332fedb3cf48c1244b345a4f963d9
Chris Luo <chris.ying.luo@gmail.com>
2018-09-01 13:50:02 -0700

    added date-fns, date transform

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -7,6 +7,22 @@ import Avatar from '@material-ui/core/Avatar';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Face from '@material-ui/icons/Face';
 import classes from './Home.css';
+import format from 'date-fns/format';
+import differenceInHours from 'date-fns/difference_in_hours';
+
+const transformDate = date => {
+    const hours = differenceInHours(new Date(), date);
+    if (hours < 12) {
+        return format(date, 'h:mm A');
+    }
+    if (hours < 24) {
+        return 'Yesterday';
+    }
+    if (hours < 72) {
+        return format(date, 'dddd');
+    }
+    return format(date, 'M/D/YY');
+}
 
 class Home extends Component {
     state = {
@@ -16,7 +32,6 @@ class Home extends Component {
 
     componentDidMount() {
         const token = localStorage.getItem('token');
-        console.log(token);
         axios.get('http://localhost:3000/users/1/chats', {
             headers: {
                 authorization: `Bearer ${token}`
@@ -62,14 +77,21 @@ class Home extends Component {
                 <List>
                     {
                         this.state.chats.map(chat => (
-                            <ListItem button key={chat.id} onClick={this.onChat(chat)}>
+                            <ListItem
+                                button
+                                key={chat.id}
+                                onClick={this.onChat(chat)}>
                                 <ListItemAvatar>
                                     <Avatar>
                                         <Face />
                                     </Avatar>
                                 </ListItemAvatar>
-                                <ListItemText primary={chat.users[1]} secondary={chat.messages[0].body} />
-                                <ListItemText secondary={chat.messages[0].send_time} className={classes.time} />
+                                <ListItemText
+                                    primary={chat.users[1]}
+                                    secondary={chat.messages[0].body} />
+                                <ListItemText
+                                    secondary={transformDate(chat.messages[0].send_time)}
+                                    className={classes.time} />
                             </ListItem>
                         ))
                     }
